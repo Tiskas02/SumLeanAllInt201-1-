5c6b14b968e7a7fbaf6dca2b9dd39343ddc49a22
TEST_INT201 <TEST_INT201@5CD3019B4C.control.ad.sit.kmutt.ac.th>
2023-09-12 10:47:51 +0700

    first

--- passwordValidation.js
+++ passwordValidation.js
@@ -1,6 +1,44 @@
 // Write your student id, firstname, and lastname in a single line comment here
+//65130500078
 function isPasswordValid(password) {
   //write your code here...
+  if(!password){
+    return false
+  }
+  let passCount =[password]
+  let letters = [...passCount.toString()] //spread array of characters
+  let msg = ''
+const arrPass = new Array()
+const arrPop = new Array()
+  for (let ch of letters) {
+    msg += ch + ''
+  }
+   arrPass.push(msg.split(''))
+console.log(msg)
+console.log(arrPass);
+
+console.log(arrPass[0][0]);
+for (let index = 0; index < arrPass.length; index++) {
+  if (arrPass[index][index] > 8) {
+    
+  }
 }
+
+ 
+
+  // for (let index = 0; index < array.length; index++) {
+  //   const element = array[index];
+    
+  // }
+
+  
+}
+console.log(isPasswordValid(555));
+// const paragraph = 'The quick brown fox jumps over the lazy dog. It barked.';
+// const regex = /[A-Z]/g;
+// const found = paragraph.match(regex);
+
+// console.log(found);
+// // Expected output: Array ["T", "I"]
 
 module.exports = isPasswordValid
